51b378a32ae8578b54ef0a09a4ca8de151f40836
gabriel <gabriel.stepien@inf.ufrgs.br>
2019-03-30 02:03:56 -0300

    adjust time

--- src/app/reservation.ts
+++ src/app/reservation.ts
@@ -9,27 +9,27 @@ export class Reservation {
     value: string;
     email: string;
   
-    // constructor() { 
-    //   this.id = null;
-    //   this.name = "";
-    //   this.cpf = "";
-    //   this.phone = "";
-    //   this.seat = "0";
-    //   this.confirmed = false;
-    //   this.value = "0";
-    //   this.email = "";
-    // }
-
     constructor() { 
       this.id = null;
-      this.name = "Teste1";
-      this.cpf = "44444444444";
-      this.phone = "44444444444";
+      this.name = "";
+      this.cpf = "";
+      this.phone = "";
       this.seat = "0";
       this.confirmed = false;
-      this.value = "1";
-      this.email = "gabriel@teste.com";
+      this.value = "0";
+      this.email = "";
     }
+
+    // constructor() { 
+    //   this.id = null;
+    //   this.name = "Teste1";
+    //   this.cpf = "44444444444";
+    //   this.phone = "44444444444";
+    //   this.seat = "0";
+    //   this.confirmed = false;
+    //   this.value = "1";
+    //   this.email = "gabriel@teste.com";
+    // }
 
 
 }
